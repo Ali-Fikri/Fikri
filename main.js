0db567d0e7b661bad93baba14fcf9a8709ebfd0c
Ali-Fikri <alifikripr@gmail.com>
2023-01-18 16:30:29 +0200

    Added JavaScript Code to Create Animation Progress Bar and Countdown Timer

---
+++ main.js
@@ -0,0 +1,50 @@
+// Global Variables
+const skillsProgress = document.querySelectorAll(".the-progress span");
+const skillsSection = document.querySelector(".our-skills");
+const countDownDate = new Date(2023,3,15,23,59,59).getTime();
+let dateNow;
+let days;
+let hours;
+let minutes;
+let seconds;
+
+
+
+// Create Progress Bar Animation
+window.onscroll = _=> {
+    if (window.scrollY >= skillsSection.offsetTop - 380) {
+        skillsProgress.forEach(span => {
+            span.style.width = span.dataset.width
+        });
+    }
+}
+
+
+// Create Countdown Timer 
+let counter = setInterval(_=> {
+    // Get Current Date
+    dateNow = new Date().getTime();
+
+    dateDiff = countDownDate - dateNow;
+
+    // Get Days
+    days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+
+    // Get Hours
+    hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+
+    // Get Minutes
+    minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60)); 
+
+    // Get Seconds
+    seconds = Math.floor((dateDiff % (1000 * 60)) / 1000); 
+
+    document.querySelector('.days').innerHTML = days < 10 ? `0${days}` : days;
+    document.querySelector('.hours').innerHTML = hours < 10 ? `0${hours}` : hours;
+    document.querySelector('.minutes').innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    document.querySelector('.seconds').innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+    if (dateDiff < 0) {
+        clearInterval(counter);
+    }
+},1000)
